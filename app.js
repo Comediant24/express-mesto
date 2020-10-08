3c40ace2072dd8614e4a6e80319b4a7ba6b1d95b
Iurii Bairamukov <juribai@mail.ru>
2020-10-08 15:56:57 +0700

    feat: содать роуты для cards, users, users/id
    feat: организовать получение карт через работу с файловой системой
    
    feat: добавить JSON файлы с данными по картам и юзерам

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cards = require('./routes/cards');
+const users = require('./routes/users');
 
 const app = express();
 const { PORT = 3000 } = process.env;
+
+app.use(cards, users);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
