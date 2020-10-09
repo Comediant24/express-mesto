9fe960267bfd6f1e33514a4eee1dd4eca0bc1870
Iurii Bairamukov <juribai@mail.ru>
2020-10-09 22:50:03 +0700

    feat: изменить чтение из JSON cards и users

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
-const cards = require('../data/cards.json');
+const fsPromises = require('fs').promises;
+const path = require('path');
 
-router.get('/cards', (req, res) => {
-  res.send(cards);
-});
+function cards(req, res) {
+  const filePath = path.join(__dirname, '..', 'data', 'cards.json');
+  fsPromises
+    .readFile(filePath, { encoding: 'utf8' })
+    .then((dataCards) => JSON.parse(dataCards))
+    .then((dataCards) => res.status(200).send(dataCards))
+    .catch((err) => console.log(err));
+}
+
+router.get('/cards', cards);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,33 @@
 const router = require('express').Router();
-const users = require('../data/users.json');
+const fsPromises = require('fs').promises;
+const path = require('path');
 
-router.get('/users', (req, res) => {
-  res.send(users);
-});
+function getAllUsers(req, res) {
+  const filePath = path.join(__dirname, '..', 'data', 'users.json');
+  fsPromises
+    .readFile(filePath, { encoding: 'utf8' })
+    .then((users) => JSON.parse(users))
+    .then((users) => res.send(users))
+    .catch((err) => console.log(err));
+}
 
-router.get('/users/:id', (req, res) => {
-  if (!users[req.params.id]) {
-    res.send({ message: 'Нет пользователя с таким id' });
-    return;
-  }
-  res.send(users[req.params.id]);
-});
+function getUserById(req, res) {
+  const filePath = path.join(__dirname, '..', 'data', 'users.json');
+  fsPromises
+    .readFile(filePath, { encoding: 'utf8' })
+    .then((users) => JSON.parse(users))
+    .then((users) => {
+      const currentUser = users.find((user) => user._id === req.params.id);
+      if (!currentUser) {
+        res.status(404).send({ message: 'Нет пользователя с таким id' });
+        return;
+      }
+      res.send(currentUser);
+    })
+    .catch((err) => console.log(err));
+}
+
+router.get('/users', getAllUsers);
+router.get('/users/:id', getUserById);
 
 module.exports = router;
