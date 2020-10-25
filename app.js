133a43e50878effcd938f4bd05231cb74e796e3f
Iurii Bairamukov <juribai@mail.ru>
2020-10-25 14:45:19 +0700

    feat: подключить MongoDB к проекту

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const cards = require('./routes/cards');
 const users = require('./routes/users');
 
 const app = express();
 const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.use(cards, users);
 
