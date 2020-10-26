c736be6c7a95f715b73cc84d41ea37d54955d7d5
Iurii Bairamukov <juribai@mail.ru>
2020-10-26 11:23:29 +0700

    feat: удалить статику

--- app.js
+++ app.js
@@ -26,8 +26,6 @@ const { PORT = 3000 } = process.env;
 
 app.use(cards, users);
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('*', (req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
